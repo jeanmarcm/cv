aed745ab657f3e687a47d2aff99f887fc33a7c30
jeanmarcm <39524460+jeanmarcm@users.noreply.github.com>
2019-05-28 21:37:31 +0200

    myCv
    Enjoy.

---
+++ cv/monscript.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+$("#footer").css("display", "none");
+$("#footer").slideUp(500).delay(500).fadeIn(0);	
+});
+
+
+$(document).ready(function(){
+$("#header").css("display", "none");
+$("#header").slideUp(1000).delay(2000).fadeIn(1000);	
+});
+
+$(document).ready(function(){
+$("#sect1").css("display", "none");
+$("#sect1").slideUp(5600).delay(3000).fadeIn(1000);	
+});
+
+
+$(document).ready(function(){
+$("#sect2").css("display", "none");
+$("#sect2").slideUp(800).delay(2000).fadeIn(1200);	
+});
+
+$(document).ready(function(){
+$("#sect3").css("display", "none");
+$("#sect3").slideUp(900).delay(2600).fadeIn(1600);	
+});
+
+$(document).ready(function(){
+$("#text1").css("display", "none");
+$("#text1").slideUp(1000).delay(3200).fadeIn(2400);	
+});
+
+$(document).ready(function(){
+$("#fin").css("display", "none");
+$("#fin").slideUp(1100).delay(3800).fadeIn(2400);	
+});
